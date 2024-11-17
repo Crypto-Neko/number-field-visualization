19f4828766aefa8210125261246799c485342061
Crypto-Neko <objective.axioma@gmail.com>
2024-11-17 14:03:04 -0500

    Removed unnecessary data for quadratic fields.

--- compute_invariants.py
+++ compute_invariants.py
@@ -2,13 +2,11 @@ from sagemath import *
 
 # Compute the invariants of the number field K.
 def compute_field(K):
-    deg = K.degree()                                # Get the degree of K over QQ.
     dK = K.discriminant()                           # Compute the discriminant of K (d_K).
     hK = K.class_number()                           # Compute the class number of K (h_K).
     G = K.unit_group(); uK = G.order()              # Compute the order of the units group of K (u_K).
-    r1 = K.real_places(); r2 = K.complex_places()   # Compute the number of real and complex places of K.
 
-    return [deg, dK, hK, uK, r1, r2]
+    return [dK, hK, uK]
 
 # Compute the field invariants of the first n real quadratic fields
 def real_quad(n):
